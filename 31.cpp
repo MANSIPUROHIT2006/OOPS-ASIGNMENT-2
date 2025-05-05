de1e3070c076e14423a2b761069b152c98fcf135
MANSI2006 <mansipurohit18@gmail.com>
2025-05-05 14:13:51 +0530

    Create 31.cpp

---
+++ 31.cpp
@@ -0,0 +1,13 @@
+// Implement a function that takes two integers and returns their product
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "Enter a number" << endl;
+    cin >> x;
+    cout << "Enter another number" << endl;
+    cin >> y;
+    cout << "The product of " << x << " and " << y << " is : " << x * y << endl;
+    return 0;
+}
