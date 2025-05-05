96177e1300ac14f614ea2f9a4cdb8e734b2484f4
MANSI2006 <mansipurohit18@gmail.com>
2025-05-05 14:10:09 +0530

    Create 26.cpp

---
+++ 26.cpp
@@ -0,0 +1,9 @@
+// Write a program that prints "Hello , World "using the main function
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello , World" << endl;
+    return 0;
+}
